68e9029d7e7ab3265816d9458e9d87ead5f20cf0
amv0107 <amv0107@gmail.com>
2024-01-01 02:29:09 +0200

    setup retrofit; adding Hilt to project

--- app/src/main/java/com/amv0107/fakestore/MainActivity.kt
+++ app/src/main/java/com/amv0107/fakestore/MainActivity.kt
@@ -2,10 +2,32 @@ package com.amv0107.fakestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
+import dagger.hilt.android.AndroidEntryPoint
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject lateinit var productService: ProductsServices
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lifecycleScope.launchWhenStarted {
+            val response = productService.getAllProducts()
+            Log.i("DATA", response.body()!!.toString())
+        }
+    }
+
+    interface ProductsServices {
+        @GET("products")
+        suspend fun getAllProducts(): Response<List<Any>>
     }
 }
